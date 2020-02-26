7a94a598804e1ca1ec17376c95d4a15b9360fe0e
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2020-02-26 08:02:24 +0100

    add BC layer for symfony/config 4.1 and older

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -14,7 +14,14 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('notify');
 
-        $treeBuilder->getRootNode()
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('notify');
+        }
+
+        $rootNode
             ->children()
                 ->scalarNode('default')
                     ->cannotBeEmpty()
@@ -23,7 +30,7 @@ class Configuration implements ConfigurationInterface
                 ->arrayNode('notifiers')
                     ->ignoreExtraKeys(false)
                     ->useAttributeAsKey('notifier')
-                    ->arrayPrototype()
+                    ->prototype('array')
                         ->isRequired()
                         ->ignoreExtraKeys(false)
                         ->performNoDeepMerging()
